0890323b134c18484c3ef2acb1ffc3c177e6d4c1
liyixuan <liyixuan4030614@gmail.com>
2020-10-04 18:32:34 -0400

    update demo3.

--- projects/project_1/code/demo/demo3_regex_parser.py
+++ projects/project_1/code/demo/demo3_regex_parser.py
@@ -1,23 +1,21 @@
 import nltk
 
-sents = 'There is 1 mln tonnes.'
-lib = sents.split('\n')[1:-1]
+sents = 'Total sales for the same period are expected to rise to 2 mln tonnes.'
 
-lib_words = []
-for l in lib:
-    if l.find(' ') == -1:
-        lib_words.append(l)
-    else:
-        lib_words.extend(l.split(' '))
-print(lib_words)
+# lib = sents.split('\n')[1:-1]
+# lib_words = []
+# for l in lib:
+#     if l.find(' ') == -1:
+#         lib_words.append(l)
+#     else:
+#         lib_words.extend(l.split(' '))
+# print(lib_words)
 
 sentences = nltk.sent_tokenize(sents)
 
 
 grammar = r"""
-    UN: {<CD> <NNS>}
-        {<CD> <NN> <NN>}
-        {<CD> <NNS> <IN> <NN>}
+    UN: {<CD> <CD|NNS> <NNS>} 
 """
 cp = nltk.RegexpParser(grammar)
 
@@ -25,9 +23,9 @@ for s in sentences:
     words = nltk.word_tokenize(s)
     pos_tags = nltk.pos_tag(words)
     print(pos_tags)
-    # tree = cp.parse(pos_tags)
+    tree = cp.parse(pos_tags)
     # print(tree)
-    # tree.draw()
+    tree.draw()
 
     # for subtree in tree.subtrees():
     #     if subtree.label() == 'UN':
